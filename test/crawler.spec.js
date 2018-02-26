1fc9cb169923e9ad78ccf504dd69ffee4c829917
Jacob Bare <jacob@limit0.io>
2018-02-26 11:41:23 -0600

    Explicitly call done

--- test/crawler.spec.js
+++ test/crawler.spec.js
@@ -14,13 +14,15 @@ describe('crawler', function() {
   });
   describe('#validate', function() {
     ['', null, undefined, false, 0, 'somestring', 'ftp://google.com', 'localhost', '//some/path', 'http://', 'https://', 'http://not-fqdn'].forEach((value) => {
-      it(`should throw an error when the url is '${value}'.`, function() {
+      it(`should throw an error when the url is '${value}'.`, function(done) {
         expect(() => crawler.validate(value)).to.throw(Error, `The provided value '${value}' is not a valid URL.`);
+        done();
       });
     });
     ['http://google.com', 'https://google.com/', 'http://www.google.com:80', 'https://www.google.com/foo?bar=baz', 'http://user:password@google.com'].forEach((value) => {
-      it(`should return true when the url is '${value}'.`, function() {
+      it(`should return true when the url is '${value}'.`, function(done) {
         expect(crawler.validate(value)).to.be.true;
+        done();
       });
     });
   });
